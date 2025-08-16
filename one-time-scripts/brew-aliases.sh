60d04348be4d4433db3ff8fc0dadd707a0724d6a
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-08-16 15:55:05 +0400

    Add comments to brew aliases

--- one-time-scripts/brew-aliases.sh
+++ one-time-scripts/brew-aliases.sh
@@ -1,2 +1,2 @@
-brew alias i='install --no-quarantine'
-brew alias uni='uninstall --zap'
+brew alias i='install --no-quarantine' # Reason: I trust Homebrew packages, and trust myself to not install malicious software.
+brew alias uni='uninstall --zap' # Use the --force if this fails due to `.app` bundle already deleted.
